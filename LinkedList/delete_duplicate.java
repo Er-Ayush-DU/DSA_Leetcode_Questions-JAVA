8429ee1a8e9688b671f01cc04aca5662ea793e07
Er-Ayush-DU <er.ayush336@gmail.com>
2025-02-08 18:17:29 +0530

    add files

---
+++ LinkedList/delete_duplicate.java
@@ -0,0 +1,50 @@
+class Node{
+  int data;
+  Node next;
+
+  Node(int data){
+    this.data = data;
+  }
+}
+
+
+public class delete_duplicate {
+  static void delete_dupl(Node head){
+      Node temp = head;
+      while(temp!=null && temp.next!= null){
+        if(temp.data == temp.next.data){
+          temp.next = temp.next.next;
+        }
+        else{
+          temp = temp.next;
+        }
+        System.out.println(temp.data + " ");
+        temp = temp.next;
+      }
+      
+    }
+  
+    // static void display(Node head){
+    //    Node temp = head;
+    //   while(temp!=null){
+    //     System.out.println(temp.data + " ");
+    //     temp = temp.next;
+    //   }
+    // }
+    public static void main(String[] args) {
+  
+  
+  
+      
+        Node head = new Node(1);
+        head.next = new Node(1);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+        head.next.next.next.next.next.next = new Node(6);
+  
+        delete_dupl(head);
+        // display(head);
+  }
+}
